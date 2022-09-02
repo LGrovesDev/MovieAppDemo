23bedb615f61fb24e3e7604da37263aa38e194ec
Logan Groves <logangrovesDev@gmail.com>
2022-09-01 22:10:08 -0700

    Will retrieve and display poster if available

--- app/src/main/java/com/example/movieappdemo/MainActivity.kt
+++ app/src/main/java/com/example/movieappdemo/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.movieappdemo
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -16,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     private var movieTitleText: TextView? = null
     private var yearReleasedText: TextView? = null
+    private var posterDisplay: ImageView? = null
 
 
     private val apiUrl = "https://www.omdbapi.com/?t="
@@ -30,13 +38,18 @@ class MainActivity : AppCompatActivity() {
         //Textviews for retrieved data to be displayed
         movieTitleText = findViewById(R.id.movie_title)
         yearReleasedText = findViewById(R.id.year_released)
+        posterDisplay = findViewById(R.id.poster_display)
 
         //Retrieve text from searchbar (user input)
         val searchText = findViewById<EditText>(R.id.SearchText)
 
         //Set up Button to listen for user search request
         val btnSearch = findViewById<Button>(R.id.search_btn)
+        //Search button ->
+        //  If valid movie title is provided, will return available title, year, poster. ->
+        //  API GET request made to OMDB API to fetch results ->
         btnSearch.setOnClickListener {
+            hideKeyboard()
             if (searchText.text.isNotEmpty()) {
                 titleRequest = searchText.text.toString().replace(" ", "+")
                 val queue = Volley.newRequestQueue(this)
@@ -44,24 +57,35 @@ class MainActivity : AppCompatActivity() {
 
                 val stringRequest = StringRequest(Request.Method.GET, url, { response ->
                     val jsonObj = JSONObject(response)
-                    val map = jsonObj.toMap()
+                    val jsonMapped = jsonObj.toMap()
 
-                    if (map["Response"]!! == "True") {
-                        movieTitleText?.text = map["Title"].toString()
-                        yearReleasedText?.text = map["Year"].toString()
-                        /*if (map["Poster"] != ("N/A")) {
-
-                        }*/
+                    //Check if the response contains a valid movie
+                    //Valid response will populate necessary fields
+                    if (jsonMapped["Response"]!! == "True") {
+                        movieTitleText?.text = jsonMapped["Title"].toString()
+                        yearReleasedText?.text = jsonMapped["Year"].toString()
+                        if (jsonMapped["Poster"] != ("N/A")) {
+                            Picasso.get().load(jsonMapped["Poster"].toString()).into(posterDisplay)
+                        } else {
+                            posterDisplay?.setImageResource(R.drawable.ic_launcher_foreground)
+                        }
                     } else {
                         movieTitleText?.text = "No Movie Found."
                         yearReleasedText?.text = "N/A"
+                        posterDisplay?.setImageResource(R.drawable.ic_launcher_foreground)
                     }
                 },
-                    { movieTitleText?.text = "FAILED" })
+                    //API error --- bad request, unable to determine if valid search or not.
+                    {
+                        movieTitleText?.text = "Unable to retrieve title"
+                        yearReleasedText?.text = "Unable to retrieve year"
+                        posterDisplay?.setImageResource(R.drawable.ic_launcher_foreground)
+                    })
+
 
                 queue.add(stringRequest)
             } else {
-                //Nothing to do here, no search provided.
+                //Nothing to do here yet, no search provided.
             }
         }
     }
@@ -85,5 +109,18 @@ class MainActivity : AppCompatActivity() {
             JSONObject.NULL -> null
             else            -> value
         }
+    }
+
+
+    //Allows to call 'hideKeyboard()' programmatically close users keyboard.
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+    private fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
